fd1afd36817279d71e88fc170c4302ee2ce0a7ab
matheusOliveira99 <matheusOliveira99@users.noreply.github.com>
2021-06-21 18:28:05 -0300

    Primeiro dia, desenvolvendo um contador

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,45 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.lime),
+      home: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meu primeiro App"),
+      ),
+      body: Center(
+        child: Text(
+          "Contador\n$count",
+          textAlign: TextAlign.center,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
+}
